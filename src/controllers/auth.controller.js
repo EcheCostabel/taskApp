a899b1baa282506ab7c1a9f347d427344de156e3
EcheCostabel <exequielcostabel@gmail.com>
2023-06-13 09:50:21 -0300

    login done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -27,12 +27,36 @@ export const register = async(req, res) => {
         })
     
     } catch (error) {
-        console.log(error)
+        res.status(500).json({message: error.message})
     }
 };
 
 
-export const login = (req, res) => {
-    res.send('Login')
+export const login = async(req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const userFound = await User.findOne({email});
+        if (!userFound) return res.status(400).json({message: 'User not found'});
+
+        const isMatch = await bcrypt.compare(password, userFound.password); //comparamos la contraseña que viene con la del usuario
+
+        if(!isMatch) return res.status(400).json({message: 'Incorrect password'});
+       
+       
+        const token = await createAccesToken({id: userFound._id});
+        res.cookie('token', token);
+        
+        res.json({      //esto es lo que me va a devolver cuando hago el post (lo que le mando al front)
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+            createdAt: userFound.createdAt,
+            updatedAt: userFound.updatedAt,
+        });
+    
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
  
 };
