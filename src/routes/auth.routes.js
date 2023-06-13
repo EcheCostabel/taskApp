6345beba0973744f6b44b95c4fd77f1863296ce2
EcheCostabel <exequielcostabel@gmail.com>
2023-06-13 11:00:41 -0300

    tasks

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import { login, logout, profile, register } from "../controllers/auth.controller.js";
+import { authRequired } from "../middlewares/validateToken.js";
+
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/logout', logout);
+
+router.get('/profile', authRequired, profile); //Cuando quiero proteger una ruta le paso un middleware 
 
 export default router;
