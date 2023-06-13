46b5bae97ccd175400b1d08b6ef3aa7174e24006
EcheCostabel <exequielcostabel@gmail.com>
2023-06-13 12:16:23 -0300

    get, post, put, delete

---
+++ src/controllers/task.controller.js
@@ -0,0 +1,56 @@
+import Task from "../models/task.model.js"
+
+
+export const getTasks = async (req, res) => {
+
+    const tasks = await Task.find({
+        user: req.user.id  //Con esto le digo que me traiga solo las tareas del user que esta logeado
+    }).populate('user');
+    res.json(tasks)
+
+}
+
+
+export const createTask = async (req, res) => {
+
+    const {title, description, date } = req.body;
+    const newTask = new Task({
+        title,
+        description,
+        date,
+        user: req.user.id
+    })
+    const savedTask = await newTask.save();
+    res.json(savedTask)
+
+}
+
+
+export const getTaskById = async (req, res) => {
+
+    const task = await Task.findById(req.params.id).populate('user')
+    if(!task) return res.status(404).send({message: 'Task not found'})
+
+    res.json(task)
+
+}
+
+
+export const deleteTask = async (req, res) => {
+
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if(!task) return res.status(404).send({message: 'Task not found'})
+
+    res.sendStatus(204)
+
+}
+
+
+export const updateTask = async (req, res) => {
+
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true});  //Para actualizar le mando como 2do parametro lo que quiero que mande y con el 3er le digo que me mande el dato nuevo, sino me manda el viejo
+    if(!task) return res.status(404).send({message: 'Task not found'})
+
+    res.json(task)
+
+}
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { authRequired } from "../middlewares/validateToken.js";
+import { createTask, deleteTask, getTaskById, getTasks, updateTask } from "../controllers/task.controller.js";
 
 
 
 const router = Router();
 
-router.get('/tasks',authRequired, ( req, res ) => res.send('tasks'));
+router.get('/tasks',authRequired, getTasks);
+router.get('/tasks/:id',authRequired, getTaskById);
+router.post('/tasks',authRequired, createTask);
+router.delete('/tasks/:id',authRequired, deleteTask);
+router.put('/tasks/:id',authRequired, updateTask);
 
 
 export default router;
